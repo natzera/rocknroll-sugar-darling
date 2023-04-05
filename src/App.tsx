6f24bbffc7c931797d318b4aafd6df94b1776ad1
Natália <nataliaclickz@gmail.com>
2023-04-05 00:10:44 -0300

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { NavBar } from "./components/NavBar";
 
+import logo from './assets/logo-pethit.png';
+
+import './styles/home.scss';
 import './styles/_global.scss';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <section>
+        <img src={logo} />
+      </section>
     </div>
   )
 }
