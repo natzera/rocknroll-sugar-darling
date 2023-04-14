33d4a399045be110b129f8b5f770b9984f1131d2
Natália <nataliaclickz@gmail.com>
2023-04-13 22:07:23 -0300

    feat(utils): music utils file

---
+++ src/utils/musics.ts
@@ -0,0 +1,48 @@
+export const MUSICS = [
+  {
+    id: '1',
+    name: 'Intro'
+  },
+  {
+    id: '2',
+    name: 'RocknRoll Sugar Darling'
+  },
+  {
+    id: '3',
+    name: 'Romeo'
+  },
+  {
+    id: '4',
+    name: 'Quero ser Seu Cão'
+  },
+  {
+    id: '5',
+    name: 'Save The Last Dance'
+  },
+  {
+    id: '6',
+    name: 'De Trago Em Trago'
+  },
+  {
+    id: '7',
+    name: '1992'
+  },
+  {
+    id: '8',
+    name: 'Honey Bi'
+  },
+  {
+    id: '9',
+    name: 'Voodoo'
+  },
+  {
+    id: '10',
+    name: 'Perdedor'
+  },
+  {
+    id: '11',
+    name: 'Story In Blue'
+  },
+
+
+]
